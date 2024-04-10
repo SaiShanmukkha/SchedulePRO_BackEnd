06442b096b102b2f4c8ebffeced34436c8575247
Surapaneni Sai Shanmukkha <90093621+SaiShanmukkha@users.noreply.github.com>
2024-04-10 03:29:32 -0500

    added new fields

--- schedulebuilder/serializers.py
+++ schedulebuilder/serializers.py
@@ -7,9 +7,12 @@ class ScheduleSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class ScheduleCourseSerializer(serializers.ModelSerializer):
+    credits = serializers.SerializerMethodField()
     class Meta:
         model = ScheduleCourse
         fields = '__all__'
+    def get_credits(self, obj):
+        return obj.course.credits if obj.course else None
 
 class ScheduleFacultySerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,11 +23,3 @@ class ScheduleSectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ScheduleSection
         fields = '__all__'
-
-# class ScheduleSerializer(serializers.ModelSerializer):
-#     courses = ScheduleCourseSerializer(many=True, read_only=True, source='schedulecourse_set')
-#     faculties = ScheduleFacultySerializer(many=True, read_only=True, source='schedulefaculty_set')
-
-#     class Meta:
-#         model = Schedule
-#         fields = ['id', 'name', 'courses', 'faculties', 'semester', 'status', 'lastUpdated', 'createdAt', 'slug']
